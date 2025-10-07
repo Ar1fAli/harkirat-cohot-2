00cd89015232c43bc53722f9b950ba183b3e2598
ar1fali <arifalu230@gmail.com>
2025-10-07 07:31:55 +0530

    fronted creating createtodo components

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,10 @@
+export function CreateTodo() {
+  return (
+    <div>
+      <h1>create todo</h1>
+      <input type="text" name="" id="" />
+      <input type="text" name="" id="" />
+      <button>Add a todos</button>
+    </div>
+  );
+}
